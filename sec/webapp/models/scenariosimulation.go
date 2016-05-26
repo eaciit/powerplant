8301b435fb5b47f05bf9e6e45e68a828af40d1a0
aryadhira <aryadhira25@gmail.com>
2016-05-26 09:56:28 +0700

    Scenario Simulation
    Add Scenario Simulation for value equation

--- sec/webapp/models/scenariosimulation.go
+++ sec/webapp/models/scenariosimulation.go
@@ -2,38 +2,49 @@ package models
 
 import (
 	"github.com/eaciit/orm"
+	"gopkg.in/mgo.v2/bson"
 	"time"
 )
 
 type SelectedScenario struct {
-	ID    string
-	Name  string
-	Value float64
+	ID    string  `bson:"ID"`
+	Name  string  `bson:"Name"`
+	Value float64 `bson:"Value"`
 }
 
 type SimulationResult struct {
-	Revenue         float64
-	LaborCost       float64
-	MaterialCost    float64
-	ServiceCost     float64
-	OperatingCost   float64
-	MaintenanceCost float64
-	ValueEquation   float64
+	Revenue         float64 `bson:"Revenue"`
+	LaborCost       float64 `bson:"LaborCost"`
+	MaterialCost    float64 `bson:"MaterialCost"`
+	ServiceCost     float64 `bson:"ServiceCost"`
+	OperatingCost   float64 `bson:"OperatingCost"`
+	MaintenanceCost float64 `bson:"MaintenanceCost"`
+	ValueEquation   float64 `bson:"ValueEquation"`
 }
 
 type ScenarioSimulationModel struct {
-	orm.ModelBase    `bson:"base"`
-	Start_Period     time.Time
-	End_Period       time.Time
-	Name             string
-	Description      string
-	SelectedPlant    []string
-	SelectedUnit     []string
-	SelectedScenario []SelectedScenario
-	HistoricResult   SimulationResult
-	FutureResult     SimulationResult
-	Differential     SimulationResult
-	Last_Update      time.Time
+	orm.ModelBase    `bson:"-",json:"-"`
+	ID               bson.ObjectId      `bson:"_id" , json:"_id"`
+	Start_Period     time.Time          `bson:"Start_Period"`
+	End_Period       time.Time          `bson:"End_Period"`
+	Name             string             `bson:"Name"`
+	Description      string             `bson:"Description"`
+	SelectedPlant    []string           `bson:"SelectedPlant"`
+	SelectedUnit     []string           `bson:"SelectedUnit"`
+	SelectedScenario []SelectedScenario `bson:"SelectedScenario"`
+	HistoricResult   SimulationResult   `bson:"HistoricResult"`
+	FutureResult     SimulationResult   `bson:"FutureResult"`
+	Differential     SimulationResult   `bson:"Differential"`
+	Last_Update      time.Time          `bson:"Last_Update"`
+}
+
+func (a *ScenarioSimulationModel) RecordID() interface{} {
+	return a.ID
+}
+
+func NewScenarioSimulation() *ScenarioSimulationModel {
+	e := new(ScenarioSimulationModel)
+	return e
 }
 
 func (c *ScenarioSimulationModel) TableName() string {
