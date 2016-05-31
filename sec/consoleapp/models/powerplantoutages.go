dab26b83b1d82c8de2d7c1139dad2389921d6068
aryadhira <aryadhira25@gmail.com>
2016-05-31 17:17:08 +0700

    add powerplantoutages data generate

--- sec/consoleapp/models/powerplantoutages.go
+++ sec/consoleapp/models/powerplantoutages.go
@@ -2,31 +2,40 @@ package models
 
 import (
 	"github.com/eaciit/orm"
+	"sync"
 	"time"
 )
 
 type PowerPlantOutagesDetails struct {
-	Id            string    `bson:"_id"`
-	UnitNo        string    `bson:"UnitNo"`
-	DepCap        string    `bson:"DepCap"`
-	MFR           string    `bson:"MFR"`
-	Model         string    `bson:"Model"`
-	Component     string    `bson:"Component"`
-	OutageType    string    `bson:"OutageType"`
-	OutageReason  string    `bson:"OutageReason"`
-	StartDate     time.Time `bson:"StartDate"`
-	FinishDate    time.Time `bson:"FinishDate"`
-	TotalHours    float64   `bson:"TotalHours"`
-	ExpFinishDate time.Time `bson:"ExpFinishDate"`
-	PlantName     string    `bson:"PlantName"`
+	sync.RWMutex
+	orm.ModelBase `bson:"-" json:"-"`
+	Id            int64     `bson:"Id" json:"Id"`
+	POId          string    `bson:"POId" json:"POId"`
+	UnitNo        string    `bson:"UnitNo" json:"UnitNo"`
+	DepCap        string    `bson:"DepCap" json:"DepCap"`
+	MFR           string    `bson:"MFR" json:"MFR"`
+	Model         string    `bson:"Model" json:"Model"`
+	Component     string    `bson:"Component" json:"Component"`
+	OutageType    string    `bson:"OutageType" json:"OutageType"`
+	OutageReason  string    `bson:"OutageReason" json:"OutageReason"`
+	StartDate     time.Time `bson:"StartDate" json:"StartDate"`
+	FinishDate    time.Time `bson:"FinishDate" json:"FinishDate"`
+	TotalHours    float64   `bson:"TotalHours" json:"TotalHours"`
+	ExpFinishDate time.Time `bson:"ExpFinishDate" json:"ExpFinishDate"`
+	PlantName     string    `bson:"PlantName" json:"PlantName"`
+}
+
+func (d *PowerPlantOutagesDetails) TableName() string {
+	return "PowerPlantOutagesDetails"
 }
 
 type PowerPlantOutages struct {
+	sync.RWMutex
 	orm.ModelBase `bson:"-" json:"-"`
-	Id            string                     `bson:"_id"`
-	Plant         string                     `bson:"Plant"`
-	Year          int                        `bson:"Year"`
-	Details       []PowerPlantOutagesDetails `bson:"Details"`
+	Id            string                     `bson:"_id" json:"Id"`
+	Plant         string                     `bson:"Plant" json:"Plant"`
+	Year          int                        `bson:"Year" json:"Year"`
+	Details       []PowerPlantOutagesDetails `bson:"Details" json:"Details"`
 }
 
 func (p *PowerPlantOutages) TableName() string {
